d63d89e4a116ca6d85606a8f2f2bb9d197c0472f
Umherirrender <umherirrender_de.wp@web.de>
2017-10-19 22:15:46 +0200

    Add alias file for new special page
    Follow-Up: Id0f0857f62250b3341c3ca41fc05a9830a20735e
    
    Change-Id: I701ed89aaa4bde8f5df2cc5234d2876a711be084

---
+++ EventBus.notranslate-alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for special pages that should not be translated.
+ *
+ * Do not add this file to translatewiki.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	// The special page is only for internal use
+	'RunSingleJob' => [ 'RunSingleJob' ],
+];
