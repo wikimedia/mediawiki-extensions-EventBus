bd53b222d6fd73dddc41d36dd31e7176a405b687
Ppchelko <ppchelko@wikimedia.org>
2018-11-07 16:42:20 +0000

    Revert "Revert "Set event datetime with microsecond resolution.""
    This reverts commit 2b7835ae21d0d4d8c2e4dd58ec4908f1fa2456ac.
    
    Change-Id: I0028ae53cbce9df303cee4ed61ce8a0d975bd471

--- includes/EventBus.php
+++ includes/EventBus.php
@@ -381,13 +381,17 @@ class EventBus {
 			$domain = $wgServerName;
 		}
 
+		$now = \DateTime::createFromFormat( 'U.u', microtime( true ) );
 		$event = [
 			'meta' => [
 				'uri'        => $uri,
 				'topic'      => $topic,
 				'request_id' => self::getRequestId(),
 				'id'         => self::newId(),
-				'dt'         => gmdate( 'c' ),
+				// HHVM doesn't support `v` in the date formatting strings
+				// and we actually want milliseconds, not microseconds.
+				// TODO: once support for HHVM is dropped - change it to milliseconds.
+				'dt'         => $now->format( 'Y-m-d\TH:i:s.uP' ),
 				'domain'     => $domain ?: $wgServerName,
 			],
 		];
