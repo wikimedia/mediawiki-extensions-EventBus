4d4e2998e362027313b1a41f6d4b8962426b44b2
Andrew Otto <acotto@gmail.com>
2019-04-18 14:58:33 -0400

    Replace binary strings in monolog generated events
    Bug: T221390
    Change-Id: I6657801ec3df8f335c8e0f0fe7c2c0336f92e356

---
+++ tests/phpunit/EventBusTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @covers EventBus
+ * @group EventBus
+ */
+class EventBusTest extends MediaWikiTestCase {
+
+	public function testReplaceBinaryValues() {
+		$stringVal = "hi there";
+		$this->assertEquals( 'hi there', EventBus::replaceBinaryValues( $stringVal ) );
+
+		$binaryVal = "\xFF\xFF\xFF\xFF";
+		$expected = "data:application/octet-stream;base64," . base64_encode( $binaryVal );
+		$this->assertEquals( $expected, EventBus::replaceBinaryValues( $binaryVal ) );
+	}
+
+	public function testReplaceBinaryValuesRecursive() {
+		$binaryVal = "\xFF\xFF\xFF\xFF";
+
+		$events = [
+			[ "k1" => "v1", "o1" => [ "ok1" => $binaryVal ] ],
+			[ "k1" => "v2", "o1" => [ "ok1" => "ov1" ] ]
+		];
+
+		$expected = [
+			[ "k1" => "v1", "o1" => [
+				"ok1" => "data:application/octet-stream;base64," . base64_encode( $binaryVal ) ]
+			],
+			[ "k1" => "v2", "o1" => [ "ok1" => "ov1" ] ]
+		];
+
+		EventBus::replaceBinaryValuesRecursive( $events );
+		$this->assertEquals( $expected, $events );
+	}
+}
