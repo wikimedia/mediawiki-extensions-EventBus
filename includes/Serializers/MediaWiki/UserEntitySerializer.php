wikimedia/mediawiki-extensions-EventBus

commit b948eb35ee7cd2894c9432ee0adc8d59d02b1c28

Lookup and add a user_central_id on serialized users
Depends on https://gitlab.wikimedia.org/repos/data-engineering/schemas-event-primary/-/merge_requests/22

Bug: T403664
Change-Id: I64abc740db4ea0663766ee6d708c0951588ac5af

includes/Serializers/MediaWiki/UserEntitySerializer.php
@@ -18,9 +18,11 @@
  * @file
  * @author Andrew Otto <otto@wikimedia.org>
  */
+
 namespace MediaWiki\Extension\EventBus\Serializers\MediaWiki;
 
 use MediaWiki\Extension\EventBus\Serializers\EventSerializer;
+use MediaWiki\User\CentralId\CentralIdLookup;
 use MediaWiki\User\UserFactory;
 use MediaWiki\User\UserGroupManager;
 use MediaWiki\User\UserIdentity;
@@ -39,8 +41,14 @@ class UserEntitySerializer {
 	private UserFactory $userFactory;
 
 	/**
+	 * @var CentralIdLookup
+	 */
+	private CentralIdLookup $centralIdLookup;
+
+	/**
 	 * @param UserFactory $userFactory
 	 * @param UserGroupManager $userGroupManager
+	 * @param CentralIdLookup $centralIdLookup
 	 */
 	public function __construct(
 		// NOTE: It would be better not to need a UserFactory
@@ -50,10 +58,12 @@ class UserEntitySerializer {
 		// and the correct way to convert a UserIdentity to a User is with a UserFactory.
 		// So either we need a UserFactory here, or in RevisionEntitySerializer.  It is more useful here.
 		UserFactory $userFactory,
-		UserGroupManager $userGroupManager
+		UserGroupManager $userGroupManager,
+		CentralIdLookup $centralIdLookup,
 	) {
 		$this->userFactory = $userFactory;
 		$this->userGroupManager = $userGroupManager;
+		$this->centralIdLookup = $centralIdLookup;
 	}
 
 	/**
@@ -84,6 +94,12 @@ class UserEntitySerializer {
 		}
 		if ( $user->isRegistered() ) {
 			$userAttrs['edit_count'] = $user->getEditCount();
+		}
+
+		// NOTE: centralIdFromLocalUser() returns 0 if the user's central id can't be obtained
+		$centralUserId = $this->centralIdLookup->centralIdFromLocalUser( $user );
+		if ( $centralUserId ) {
+			$userAttrs['user_central_id'] = $centralUserId;
 		}
 
 		return $userAttrs;

includes/MediaWikiEventSubscribers/PageChangeEventIngress.php
@@ -20,7 +20,7 @@
  * @author Gabriele Modena <gmodena@wikimedia.org>
  */
 
-declare( strict_types = 1 );
+declare( strict_types=1 );
 
 namespace MediaWiki\Extension\EventBus\MediaWikiEventSubscribers;
 
@@ -58,6 +58,7 @@ use MediaWiki\Page\WikiPage;
 use MediaWiki\Revision\RevisionStore;
 use MediaWiki\Storage\PageUpdateCauses;
 use MediaWiki\Title\TitleFormatter;
+use MediaWiki\User\CentralId\CentralIdLookup;
 use MediaWiki\User\UserFactory;
 use MediaWiki\User\UserGroupManager;
 use Psr\Log\LoggerInterface;
@@ -128,7 +129,8 @@ class PageChangeEventIngress extends DomainEventIngress implements
 		RevisionStore $revisionStore,
 		ContentHandlerFactory $contentHandlerFactory,
 		RedirectLookup $redirectLookup,
-		PageLookup $pageLookup
+		PageLookup $pageLookup,
+		CentralIdLookup $centralIdLookup,
 	) {
 		$this->logger = LoggerFactory::getInstance( 'EventBus.PageChangeEventIngress' );
 
@@ -138,7 +140,7 @@ class PageChangeEventIngress extends DomainEventIngress implements
 
 		$this->eventBusFactory = $eventBusFactory;
 
-		$userEntitySerializer = new UserEntitySerializer( $userFactory, $userGroupManager );
+		$userEntitySerializer = new UserEntitySerializer( $userFactory, $userGroupManager, $centralIdLookup );
 
 		$this->pageChangeEventSerializer = new PageChangeEventSerializer(
 			new EventSerializer( $mainConfig, $globalIdGenerator, Telemetry::getInstance() ),
@@ -181,8 +183,10 @@ class PageChangeEventIngress extends DomainEventIngress implements
 			$redirectLinkTarget = $page->getRedirectTarget();
 		} else {
 			$redirectSourcePageReference =
-				$pageLookup->getPageByReference( $page,
-					\Wikimedia\Rdbms\IDBAccessObject::READ_LATEST );
+				$pageLookup->getPageByReference(
+					$page,
+					\Wikimedia\Rdbms\IDBAccessObject::READ_LATEST
+				);
 
 			$redirectLinkTarget =
 				$redirectSourcePageReference != null && $redirectSourcePageReference->isRedirect()
@@ -245,15 +249,30 @@ class PageChangeEventIngress extends DomainEventIngress implements
 			$revisionRecord = $event->getLatestRevisionAfter();
 
 			$redirectTarget =
-				self::lookupRedirectTarget( $event->getPage(), $this->pageLookup,
-					$this->redirectLookup );
+				self::lookupRedirectTarget(
+					$event->getPage(),
+					$this->pageLookup,
+					$this->redirectLookup
+				);
 
 			$pageChangeEvent = $event->isCreation()
-				? $this->pageChangeEventSerializer->toCreateEvent( $this->streamName, $event->getPage(),
-					$performer, $revisionRecord, $redirectTarget )
-				: $this->pageChangeEventSerializer->toEditEvent( $this->streamName, $event->getPage(),
-					$performer, $revisionRecord, $redirectTarget,
-					$this->revisionStore->getRevisionById( $event->getPageRecordBefore()->getLatest() ) );
+				? $this->pageChangeEventSerializer->toCreateEvent(
+					$this->streamName,
+					$event->getPage(),
+					$performer,
+					$revisionRecord,
+					$redirectTarget
+				)
+				: $this->pageChangeEventSerializer->toEditEvent(
+					$this->streamName,
+					$event->getPage(),
+					$performer,
+					$revisionRecord,
+					$redirectTarget,
+					$this->revisionStore->getRevisionById(
+						$event->getPageRecordBefore()->getLatest()
+					)
+				);
 
 			$this->sendEvents( $this->streamName, [ $pageChangeEvent ] );
 		}
@@ -361,14 +380,18 @@ class PageChangeEventIngress extends DomainEventIngress implements
 		$performer = $this->userFactory->newFromUserIdentity( $event->getPerformer() );
 
 		$redirectTarget =
-			self::lookupRedirectTarget( $event->getPageRecordAfter(), $this->pageLookup,
-				$this->redirectLookup );
+			self::lookupRedirectTarget(
+				$event->getPageRecordAfter(), $this->pageLookup,
+				$this->redirectLookup
+			);
 
 		// The parentRevision is needed since a page move creates a new revision.
 		$revision = $this->revisionStore->getRevisionById(
-			$event->getPageRecordAfter()->getLatest() );
+			$event->getPageRecordAfter()->getLatest()
+		);
 		$parentRevision = $this->revisionStore->getRevisionById(
-			$event->getPageRecordBefore()->getLatest() );
+			$event->getPageRecordBefore()->getLatest()
+		);
 
 		$event = $this->pageChangeEventSerializer->toMoveEvent(
 			$this->streamName,
@@ -398,8 +421,11 @@ class PageChangeEventIngress extends DomainEventIngress implements
 			$performer = $this->userFactory->newFromUserIdentity( $event->getPerformer() );
 
 			$redirectTarget =
-				self::lookupRedirectTarget( $event->getPageRecordAfter(), $this->pageLookup,
-					$this->redirectLookup );
+				self::lookupRedirectTarget(
+					$event->getPageRecordAfter(),
+					$this->pageLookup,
+					$this->redirectLookup
+				);
 
 			// TODO: replace with $event->getPageRecordBefore()?->getId();
 			//  once EventBus CI fully adopts php 8.
@@ -473,7 +499,8 @@ class PageChangeEventIngress extends DomainEventIngress implements
 					"Current revision $revId's' visibility did not match the expected " .
 					'visibility change provided by hook. Current revision visibility is ' .
 					$revisionRecord->getVisibility() . '. visibility changed to ' .
-					$event->getVisibilityAfter( $revId ) );
+					$event->getVisibilityAfter( $revId )
+				);
 			}
 
 			// We only need to emit an event if visibility has actually changed.
@@ -499,10 +526,14 @@ class PageChangeEventIngress extends DomainEventIngress implements
 				$performerForEvent = $event->isSuppressed() ? null : $performer;
 
 				$event =
-					$this->pageChangeEventSerializer->toVisibilityChangeEvent( $this->streamName,
-						$event->getPage(), $performerForEvent, $revisionRecord,
+					$this->pageChangeEventSerializer->toVisibilityChangeEvent(
+						$this->streamName,
+						$event->getPage(),
+						$performerForEvent,
+						$revisionRecord,
 						$event->getVisibilityBefore( $revId ),
-						$event->getEventTimestamp()->getTimestamp() );
+						$event->getEventTimestamp()->getTimestamp()
+					);
 
 				$this->sendEvents( $this->streamName, [ $event ] );
 			}

includes/Serializers/MediaWiki/PageChangeEventSerializer.php
@@ -40,7 +40,7 @@ class PageChangeEventSerializer {
 	 * All page change events will have their $schema URI set to this.
 	 * https://phabricator.wikimedia.org/T308017
 	 */
-	public const PAGE_CHANGE_SCHEMA_URI = '/mediawiki/page/change/1.2.0';
+	public const PAGE_CHANGE_SCHEMA_URI = '/mediawiki/page/change/1.3.0';
 
 	/**
 	 * There are many kinds of changes that can happen to a MediaWiki pages,

tests/phpunit/integration/PageChangeEmissionTest.php
@@ -9,6 +9,7 @@ use MediaWiki\Extension\EventBus\EventBus;
 use MediaWiki\Extension\EventBus\EventBusFactory;
 use MediaWiki\Extension\EventBus\EventFactory;
 use MediaWiki\Extension\EventBus\MediaWikiEventSubscribers\PageChangeEventIngress;
+use MediaWiki\Extension\EventBus\Serializers\MediaWiki\PageChangeEventSerializer;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Page\ProperPageIdentity;
 use MediaWiki\Revision\RevisionRecord;
@@ -203,8 +204,8 @@ class PageChangeEmissionTest extends \MediaWikiIntegrationTestCase {
 	 */
 	private static function provideStreamName(): array {
 		return [
-		"Test Domain Event code paths" =>
-			[ PageChangeEventIngress::PAGE_CHANGE_STREAM_NAME_DEFAULT ]
+			"Test Domain Event code paths" =>
+				[ PageChangeEventIngress::PAGE_CHANGE_STREAM_NAME_DEFAULT ]
 		];
 	}
 
@@ -633,7 +634,7 @@ class PageChangeEmissionTest extends \MediaWikiIntegrationTestCase {
 
 	private static function assertIsValidPageChangeSchemaAndWiki( array $event ): void {
 		Assert::assertEquals( WikiMap::getCurrentWikiId(), $event['wiki_id'] );
-		Assert::assertEquals( '/mediawiki/page/change/1.2.0', $event['$schema'] );
+		Assert::assertEquals( PageChangeEventSerializer::PAGE_CHANGE_SCHEMA_URI, $event['$schema'] );
 	}
 
 	private static function assertIsValidPageChangePage( array $event, Title $pageTitle, int $ns ): void {
@@ -757,50 +758,76 @@ class PageChangeEmissionTest extends \MediaWikiIntegrationTestCase {
 		);
 
 		Assert::assertArrayHasKey( "prior_state", $destinationEvent );
-		Assert::assertSame( $sourceEvent['page']['page_title'],
-			$destinationEvent['prior_state']['page']['page_title'] );
-		Assert::assertSame( $sourceEvent['page']['namespace_id'],
-			$destinationEvent['prior_state']['page']['namespace_id'] );
+		Assert::assertSame(
+			$sourceEvent['page']['page_title'],
+			$destinationEvent['prior_state']['page']['page_title']
+		);
+		Assert::assertSame(
+			$sourceEvent['page']['namespace_id'],
+			$destinationEvent['prior_state']['page']['namespace_id']
+		);
 
 		// The moved page carries information about the parent revision's prior state.
 		// Some fields. like edit_count of an editor subkey, might diverge. Here we explicitly
 		// assert on
 		// the fields explicitly set by the `PageChangeEventSerializer::toMoveEvent` method.
 
-		Assert::assertSame( $sourceEvent['revision']['rev_id'],
-			$destinationEvent['prior_state']['revision']['rev_id'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['rev_id'],
+			$destinationEvent['prior_state']['revision']['rev_id']
+		);
 
-		Assert::assertSame( $sourceEvent['revision']['rev_dt'],
-			$destinationEvent['prior_state']['revision']['rev_dt'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['rev_dt'],
+			$destinationEvent['prior_state']['revision']['rev_dt']
+		);
 
-		Assert::assertSame( $sourceEvent['revision']['is_minor_edit'],
-			$destinationEvent['prior_state']['revision']['is_minor_edit'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['is_minor_edit'],
+			$destinationEvent['prior_state']['revision']['is_minor_edit']
+		);
 
-		Assert::assertSame( $sourceEvent['revision']['rev_sha1'],
-			$destinationEvent['prior_state']['revision']['rev_sha1'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['rev_sha1'],
+			$destinationEvent['prior_state']['revision']['rev_sha1']
+		);
 
-		Assert::assertSame( $sourceEvent['revision']['rev_size'],
-			$destinationEvent['prior_state']['revision']['rev_size'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['rev_size'],
+			$destinationEvent['prior_state']['revision']['rev_size']
+		);
 
 		if ( isset( $sourceEvent['revision']['comment'] ) ) {
-			Assert::assertSame( $sourceEvent['revision']['comment'],
-				$destinationEvent['prior_state']['revision']['comment'] );
+			Assert::assertSame(
+				$sourceEvent['revision']['comment'],
+				$destinationEvent['prior_state']['revision']['comment']
+			);
 		}
-		Assert::assertSame( $sourceEvent['revision']['editor']['is_temp'],
-			$destinationEvent['prior_state']['revision']['editor']['is_temp'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['editor']['is_temp'],
+			$destinationEvent['prior_state']['revision']['editor']['is_temp']
+		);
 
-		Assert::assertSame( $sourceEvent['revision']['editor']['user_id'],
-			$destinationEvent['prior_state']['revision']['editor']['user_id'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['editor']['user_id'],
+			$destinationEvent['prior_state']['revision']['editor']['user_id']
+		);
 
-		Assert::assertSame( $sourceEvent['revision']['editor']['edit_count'],
-			$destinationEvent['prior_state']['revision']['editor']['edit_count'] - 1 );
+		Assert::assertSame(
+			$sourceEvent['revision']['editor']['edit_count'],
+			$destinationEvent['prior_state']['revision']['editor']['edit_count'] - 1
+		);
 
-		Assert::assertSame( $sourceEvent['revision']['editor']['registration_dt'],
-			$destinationEvent['prior_state']['revision']['editor']['registration_dt'] );
+		Assert::assertSame(
+			$sourceEvent['revision']['editor']['registration_dt'],
+			$destinationEvent['prior_state']['revision']['editor']['registration_dt']
+		);
 
 		if ( isset( $sourceEvent['revision']['content_slots'] ) ) {
-			Assert::assertSame( $sourceEvent['revision']['content_slots'],
-				$destinationEvent['prior_state']['revision']['content_slots'] );
+			Assert::assertSame(
+				$sourceEvent['revision']['content_slots'],
+				$destinationEvent['prior_state']['revision']['content_slots']
+			);
 		}
 	}
 }

tests/phpunit/integration/Serializers/MediaWiki/PageChangeEventSerializerTest.php
@@ -96,7 +96,8 @@ class PageChangeEventSerializerTest extends MediaWikiIntegrationTestCase {
 
 		$this->userEntitySerializer = new UserEntitySerializer(
 			$this->userFactory,
-			$this->getServiceContainer()->getUserGroupManager()
+			$this->getServiceContainer()->getUserGroupManager(),
+			$this->getServiceContainer()->getCentralIdLookup(),
 		);
 
 		$revisionSlotEntitySerializer = new RevisionSlotEntitySerializer(

tests/phpunit/integration/Serializers/MediaWiki/RevisionEntitySerializerTest.php
@@ -45,7 +45,8 @@ class RevisionEntitySerializerTest extends MediaWikiIntegrationTestCase {
 
 		$this->userEntitySerializer = new UserEntitySerializer(
 			$this->getServiceContainer()->getUserFactory(),
-			$this->getServiceContainer()->getUserGroupManager()
+			$this->getServiceContainer()->getUserGroupManager(),
+			$this->getServiceContainer()->getCentralIdLookup(),
 		);
 
 		$this->contentEntitySerializer = new RevisionSlotEntitySerializer(

tests/phpunit/integration/Serializers/MediaWiki/UserEntitySerializerTest.php
@@ -2,6 +2,7 @@
 
 use MediaWiki\Extension\EventBus\Serializers\EventSerializer;
 use MediaWiki\Extension\EventBus\Serializers\MediaWiki\UserEntitySerializer;
+use MediaWiki\MainConfigNames;
 use MediaWiki\User\UserIdentityValue;
 
 /**
@@ -49,10 +50,18 @@ class UserEntitySerializerTest extends MediaWikiIntegrationTestCase {
 			return;
 		}
 
+		$this->overrideConfigValues( [
+			// We don't want to test specifically the CentralAuth implementation
+			// of the CentralIdLookup. As such, force it to be the local provider.
+			MainConfigNames::CentralIdLookupProvider => 'local',
+		] );
+
 		$userFactory = $this->getServiceContainer()->getUserFactory();
+		$centralIdLookup = $this->getServiceContainer()->getCentralIdLookup();
 		$this->userEntitySerializer = new UserEntitySerializer(
 			$userFactory,
-			$this->getServiceContainer()->getUserGroupManager()
+			$this->getServiceContainer()->getUserGroupManager(),
+			$centralIdLookup
 		);
 		// This is stupid workaround that allows us to declare a dataProider with
 		// the arguments initialized dynamically using getServiceContainer.
@@ -93,7 +102,8 @@ class UserEntitySerializerTest extends MediaWikiIntegrationTestCase {
 					'is_temp' => false,
 					'user_id' => $regUser->getId(),
 					'registration_dt' => EventSerializer::timestampToDt( $regUser->getRegistration() ),
-					'edit_count' => $regUser->getEditCount()
+					'edit_count' => $regUser->getEditCount(),
+					'user_central_id' => $centralIdLookup->centralIdFromLocalUser( $regUser ),
 				]
 			],
 		];

tests/phpunit/unit/PageChangeEventIngressTest.php
@@ -25,6 +25,7 @@ use MediaWiki\Storage\EditResult;
 use MediaWiki\Storage\PageUpdateCauses;
 use MediaWiki\Storage\RevisionSlotsUpdate;
 use MediaWiki\Title\Title;
+use MediaWiki\User\CentralId\CentralIdLookup;
 use MediaWiki\User\UserFactory;
 use MediaWiki\User\UserGroupManager;
 use MediaWiki\User\UserIdentity;
@@ -77,6 +78,9 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 
 	/** @var PageLookup */
 	private $pageLookup;
+
+	/** @var CentralIdLookup */
+	private $centralIdLookup;
 
 	/**
 	 * @var EventBus
@@ -141,6 +145,7 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 		$this->titleFormatter = $this->createMock( TitleFormatter::class );
 		$this->userFactory = $this->createMock( UserFactory::class );
 		$this->revisionStore = $this->createMock( RevisionStore::class );
+		$this->centralIdLookup = $this->createMock( CentralIdLookup::class );
 
 		$objectFactory = $this->createMock( ObjectFactory::class );
 		$hookContainer = $this->createMock( HookContainer::class );
@@ -151,10 +156,12 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 				return $this->createMock( $spec['class'] );
 			} );
 
-		$this->contentHandlerFactory = new ContentHandlerFactory( $handlerSpecs,
+		$this->contentHandlerFactory = new ContentHandlerFactory(
+			$handlerSpecs,
 			$objectFactory,
 			$hookContainer,
-			$logger );
+			$logger
+		);
 
 		$this->redirectLookup = $this->createMock( RedirectLookup::class );
 		$this->pageLookup = $this->createMock( PageLookup::class );
@@ -198,9 +205,10 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 			'revisionStore' => $this->revisionStore ?? $this->createMock( RevisionStore::class ),
 			'contentHandlerFactory' => $this->contentHandlerFactory ?? $this->createMock(
 				IContentHandlerFactory::class
-				),
+			),
 			'redirectLookup' => $this->redirectLookup ?? $this->createMock( RedirectLookup::class ),
-			'pageLookup' => $this->pageLookup ?? $this->createMock( PageLookup::class )
+			'pageLookup' => $this->pageLookup ?? $this->createMock( PageLookup::class ),
+			'centralIdLookup' => $this->centralIdLookup ?? $this->createMock( CentralIdLookup::class ),
 		];
 		$deps = array_merge( $defaults, $overrides );
 		return new PageChangeEventIngress(
@@ -214,7 +222,8 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 			$deps['revisionStore'],
 			$deps['contentHandlerFactory'],
 			$deps['redirectLookup'],
-			$deps['pageLookup']
+			$deps['pageLookup'],
+			$deps['centralIdLookup'],
 		);
 	}
 
@@ -432,9 +441,11 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 
 				$event = $events[0];
 
-				Assert::assertArrayHasKey( 'performer',
+				Assert::assertArrayHasKey(
+					'performer',
 					$event,
-					'Suppressed deletion should include performer information' );
+					'Suppressed deletion should include performer information'
+				);
 			} );
 
 		$listener = $this->newListenerWithOverrides();
@@ -477,9 +488,11 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 
 				$event = $events[0];
 
-				Assert::assertArrayNotHasKey( 'performer',
+				Assert::assertArrayNotHasKey(
+					'performer',
 					$event,
-					'Suppressed deletion should not include performer information' );
+					'Suppressed deletion should not include performer information'
+				);
 			} );
 
 		$listener = $this->newListenerWithOverrides();
@@ -497,13 +510,15 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 		$performer->method( 'getId' )->willReturn( 555 );
 
 		$pageRecordBefore = $this->createMockPageRecord( 456, 0, 'OldTitle', true, [ 'getLatest' => 7889 ] );
-		$pageRecordAfter = $this->createMockPageRecord( 456,
+		$pageRecordAfter = $this->createMockPageRecord(
+			456,
 			0,
 			'NewTitle',
 			true, [
 				'exists' => true,
 				'getLatest' => 7890
-			] );
+			]
+		);
 		$redirectPage = null;
 
 		$event = new PageMovedEvent(
@@ -551,10 +566,12 @@ class PageChangeEventIngressTest extends MediaWikiUnitTestCase {
 		$performer = $this->createMock( UserIdentity::class );
 
 		$pageRecordBefore = $this->createMockPageRecord( 456, 0, 'OldTitle' );
-		$pageRecordAfter = $this->createMockPageRecord( 457,
+		$pageRecordAfter = $this->createMockPageRecord(
+			457,
 			0,
 			'NewTitle',
-			true, [ 'exists' => false ] );
+			true, [ 'exists' => false ]
+		);
 
 		$event = $this->createMock( PageMovedEvent::class );
 		$event->method( 'getPageRecordBefore' )->willReturn( $pageRecordBefore );

commit d9602e9bc25847eedbdd9ff22b2ccfff34e24af8

Mark UserEntitySerializer as newable
Why:
* b948eb35ee7cd2894c9432ee0adc8d59d02b1c28 broke WikimediaEvents
  CI because the UserEntitySerializer class constructor was
  updated to take another parameter and WikimediaEvents was not
  updated for this change.
** Without noticing this before the train split, this issue would
   have become a train blocker.
* To avoid the chance of a similar breakage in the future, the
  UserEntitySerializer class should be marked as newable so that
  is is covered by the stable interface policy and future changes
  are made in-line with the policy.

What:
* Mark UserEntitySerializer as newable

Bug: T404577
Change-Id: If84eaf308954bf70cab2c628ddcbe7048aa6d29d

includes/Serializers/MediaWiki/UserEntitySerializer.php
@@ -29,6 +29,7 @@ use MediaWiki\User\UserIdentity;
 
 /**
  * Converts a User to an array matching the fragment/mediawiki/state/entity/user schema
+ * @newable - used by WikimediaEvents
  */
 class UserEntitySerializer {
 	/**